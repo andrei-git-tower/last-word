9de1a6eb60bfd680fa54bfcb2a13327e5f3ff955
Filip Persson <filip@git-tower.com>
2026-02-27 17:16:37 +0100

    Remove console logs used for debugging

--- src/pages/Widget.tsx
+++ src/pages/Widget.tsx
@@ -104,7 +104,6 @@ export default function Widget() {
         </div>
       </div>
 
-
       {/* Chat / Survey / Typeform */}
       <div
         className="flex-1 min-h-0 overflow-hidden flex flex-col"
